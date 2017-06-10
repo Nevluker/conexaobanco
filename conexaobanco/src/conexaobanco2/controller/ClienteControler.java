a7f31115e45a4f61e090b71c559f7cffa48bb954
Administrador <Administrador@10.10.3.12>
2017-06-10 11:54:40 -0300

    versao 1.3

--- conexaobanco/src/conexaobanco2/controller/ClienteControler.java
+++ conexaobanco/src/conexaobanco2/controller/ClienteControler.java
@@ -8,24 +8,28 @@ import java.util.logging.Logger;
 
 public class ClienteControler {
 
-    public boolean inserir() {
-        String sql = "INSERT INTO Cliente(nome, codigo) VALUES ('jOLE', 7)";
+    public boolean inserir(int codigo, String nm) {
+        String sql = "INSERT INTO Cliente(nome, codigo) VALUES (?, ?)";
         PreparedStatement ps;
         try {
-            ps = Conexaobanco.getConexao().prepareStatement(sql);
-            ps.execute();
+            ps = Conexaobanco.getConexao().prepareStatement(sql);//prepara a instrução SQL
+            ps.setString(1, nm);//PRIMEIRO PARAMETRO INDICA A ? correspondente, seguindo a variavel que subtituira a ?
+            ps.setInt(2, codigo);//PRIMEIRO PARAMETRO INDICA A ? correspondente, seguindo a variavel que subtituira a ?
+            ps.execute();//executa SQL
             return true;
         } catch (SQLException | ClassNotFoundException ex) {
             Logger.getLogger(ClienteControler.class.getName()).log(Level.SEVERE, null, ex);
             return false;
-
         }
 
     }
 
     public static void main(String[] args) {
         ClienteControler novo = new ClienteControler();
-        novo.inserir();
+        
+        novo.inserir(11, "pietra");
+        novo.inserir(14, "sara");
+        novo.inserir(13, "ju");
     }
 
 }
